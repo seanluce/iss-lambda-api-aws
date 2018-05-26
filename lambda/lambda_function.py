a5df5048021e108f813e207a109e2674c50a16ae
Sean Luce <lucesean@gmail.com>
2018-05-26 13:43:56 -0400

    added dynamodb logging

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,11 +1,29 @@
 import requests
 import json
+import boto3 #needed for DynamoDB
+import time
 
 def lambda_handler(event, context):
     response = requests.get("http://api.open-notify.org/iss-now.json")
     data = response.json()
+    
+    dbClient = boto3.resource('dynamodb')
+    dbTable = dbClient.Table('ISShistory')
+    
     lat = (data["iss_position"]["latitude"])
     long = (data["iss_position"]["longitude"])
+    
+    epochtime = time.time()
+    
+    dbTable.put_item(
+            Item={
+                'ID': str(epochtime),
+                'latitude': lat,
+                'longitude': long
+            }
+        
+        ) 
+    
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json" , "Access-Control-Allow-Origin": "*"},
